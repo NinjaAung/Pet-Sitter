e9187d0734019f086f5d3e6e0b0a70b27a751821
ninjaaung <ninja.aung02@gmail.com>
2020-05-15 21:28:43 -0700

    Host added

--- pet_sitter/settings.py
+++ pet_sitter/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3(dtm6$%1g^m5z#&_#kyr8w=2i52pqvg+2#-3)=bcff!(jxn12'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'pet-sitters.herokuapp.com']
 
 
 # Application definition
@@ -123,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
