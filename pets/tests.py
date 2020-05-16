630b7caea3380543a61d6ef8ac9adfa3b034dc48
ninjaaung <ninja.aung02@gmail.com>
2020-05-15 20:54:10 -0700

    User login Test

--- pets/tests.py
+++ pets/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.utils import timezone
+import datetime
 
-# Create your tests here.
+from pets.models import Pet, Appointment
+
+
+class UserTest(TestCase):
+    def user_login(self):
+        user = User.objects.create_user(
+            username='testing', 
+            password='testing'
+            )
+
+        self.client.login(
+            username='testing', 
+            password='testing')
+        
